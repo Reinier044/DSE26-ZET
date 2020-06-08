147bcb58876deacd4f073a3d57cc0437941891c2
willem505 <48433345+willem505@users.noreply.github.com>
2020-06-08 12:28:19 +0200

    New clean file

--- Power_new.py
+++ Power_new.py
@@ -1,5 +1,6 @@
+import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.gridspec as gridspec
 # ----------------------------------------------------- Functions ------------------------------------------------------
 
 
@@ -18,15 +19,17 @@ def EGTS_power(a, v, ratio, pow_wheel=2):
     m_car           = 20000     # [kg] Weight of external vehicle
     m_tot = m_plane + m_car
     weight_ratio    = 0.952     # [-] Weight distribution ratio
+    Roll_fric       = 0.02      # [-] Rolling friction coefficient
 
-    F_tot = m_tot*a  # [N] Total force req to move plane at acceleration
+    N_mlg   =  m_plane*weight_ratio*9.81
+    N_mlg_w =  N_mlg/4
+
+    F_tot = m_tot*a + Roll_fric*m_tot*9.81  # [N] Total force req to move plane at acceleration
     F_mlg = (1-ratio)*F_tot  # [N] Force needed  from internal
     F_mlg_w = F_mlg/pow_wheel  # [N] Force needed  from internal per wheel
 
     T_mlg_w = F_mlg_w*w_rad_air
 
-    N_mlg   =  m_plane*weight_ratio*9.81
-    N_mlg_w =  N_mlg/4
     if stat_traction(T_mlg_w, N_mlg_w, w_rad_air):
         print("Static friction checked")
     else:
@@ -43,17 +46,20 @@ def car_power(a, v, ratio, pow_wheel=4):
     m_car           = 20000     # [kg] Weight of external vehicle
     m_tot = m_plane + m_car
     weight_ratio    = 0.952     # [-] Weight distribution ratio
+    Roll_fric       = 0.02      # [-] Rolling friction coefficient
 
-    F_tot = m_tot*a  # [N] Total force req to move plane at acceleration
+    N_nlg   = (m_car + m_plane*(1-weight_ratio))*9.81
+    N_nlg_w = N_nlg/4
+
+    F_tot = m_tot*a + Roll_fric*m_tot*9.81  # [N] Total force req to move plane at acceleration
     F_nlg = ratio*F_tot  # [N] Force needed  from internal
+    print(F_nlg)
     F_nlg_w = F_nlg/pow_wheel  # [N] Force needed  from internal per wheel
 
     T_nlg_w_1 = F_nlg_w*w_rad_car_1
     T_nlg_w_2 = F_nlg_w*w_rad_car_2
 
 
-    N_nlg   = (m_car + m_plane*(1-weight_ratio))*9.81
-    N_nlg_w = N_nlg/4
     if stat_traction(T_nlg_w_1, N_nlg_w, w_rad_car_1):
         print("Static friction checked rear wheels.")
     else:
@@ -65,15 +71,20 @@ def car_power(a, v, ratio, pow_wheel=4):
 
     w_1 = v/w_rad_car_1
     w_2 = v/w_rad_car_2
-    return T_nlg_w_1*w_1, T_nlg_w_2*w_2
+    return T_nlg_w_1*w_1,T_nlg_w_2*w_2
 
-def s_v_a_plotter(time, displacement, velocity, acceleration, layout='v'):
+
+def s_v_a_plotter(title,time, power, velocity, acceleration, layout='v'):
     """
+    :param title: title of plots
+        'egts': On aircraft power
+        'car': Car power
+        ' ': No title
     :param layout: layout of plots
         'v': vertical
         'h': horizontal
     :param time: time array
-    :param displacement: displacement array
+    :param power: power array
     :param velocity: velocity array
     :param acceleration: acceleration array
     :return: nothing
@@ -84,26 +95,66 @@ def s_v_a_plotter(time, displacement, velocity, acceleration, layout='v'):
     else:
         n = 310
 
-    plt.figure(figsize=(20,10))
+    gs = gridspec.GridSpec(2, 2)
 
-    plt.subplot(n+1)
-    plt.title("Displacement")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Displacement [m]")
-    plt.plot(time, displacement)
+    fig = plt.figure()
+    if title is 'egts':
+        fig.suptitle("On Aircraft Power")
+        ax1 = fig.add_subplot(gs[1, :])
+        ax1.set_title("Power")
+        ax1.set_xlabel("Time [s]")
+        ax1.set_ylabel("Power [kW]")
+        ax1.plot(time, [i/1000 for i in power])
+        ax0.plot(time, [52 for i in time], color='grey', linestyle='--')
+    elif title is 'car':
+        fig.suptitle("Vehicle Power")
+        ax0 = fig.add_subplot(gs[1, 0])
+        ax0.set_title("Power Front Wheel")
+        ax0.set_xlabel("Time [s]")
+        ax0.set_ylabel("Power [kW]")
+        ax0.plot(time, [i/1000 for i in power[0, :]])
+        ax1 = fig.add_subplot(gs[1, 1])
+        ax1.set_title("Power Rear Wheel")
+        ax1.set_xlabel("Time [s]")
+        ax1.set_ylabel("Power [kW]")
+        ax1.plot(time, [i/1000 for i in power[1, :]])
 
-    plt.subplot(n+2)
-    plt.title("Velocity")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Velocity [m/s]")
-    plt.plot(time, velocity)
 
-    plt.subplot(n+3)
-    plt.title("Acceleration")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Acceleration [m/s/s]")
-    plt.plot(time, acceleration)
+    ax2 = fig.add_subplot(gs[0, 0])
+    ax2.set_title("Velocity")
+    ax2.set_xlabel("Time [s]")
+    ax2.set_ylabel("Velocity [m/s]")
+    ax2.plot(time, velocity, color='g')
+
+    ax3 = fig.add_subplot(gs[0, 1])
+    ax3.set_title("Acceleration")
+    ax3.set_xlabel("Time [s]")
+    ax3.set_ylabel("Acceleration [$m/s^2$]")
+    ax3.set_ylim(0, max(acceleration)+0.2)
+    ax3.plot(time, acceleration, color='r')
 
     plt.tight_layout()
     plt.show()
     pass
+
+def v_t(a,t):
+    v = [0]
+    for i in range(1,len(a)):
+        v.append(a[i]*(t[i]-t[i-1]) + v[i-1])
+    return v
+
+power_ratio = 0.7
+a = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0.75, 0.75, 0.75, 0.75, 0.6, 0.6, 0.5, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.2, 0.2]
+t = [i for i in range((len(a)))]
+v = v_t(a, t)
+
+P_egts_w = []
+for i in range(len(a)):
+    P_egts_w.append(EGTS_power(a[i], v[i], power_ratio))
+
+P_car_w = np.zeros((2,len(a)))
+for i in range(len(a)):
+    P_car_w[0,i], P_car_w[1,i] = car_power(a[i], v[i], power_ratio)
+
+s_v_a_plotter('egts',t, P_egts_w, v, a)
+s_v_a_plotter('car',t, P_car_w, v, a)
